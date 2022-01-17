61e815a495bb2bc03861cac3259d23076634acc4
nickgvard <project.3players@gmail.com>
2022-01-17 09:43:46 +0300

    Init commit

---
+++ src/main/java/my/education/iexcloudapidemo/IexCloudApiDemoApplication.java
@@ -0,0 +1,13 @@
+package my.education.iexcloudapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IexCloudApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IexCloudApiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/education/iexcloudapidemo/IexCloudApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package my.education.iexcloudapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IexCloudApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
